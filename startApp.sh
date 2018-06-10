2dddc9b9addc0d187b8a17a7eb2e153c55b80ccd
cooksey <suitcase24@gmail.com>
2018-06-10 17:01:20 -0400

    The cache is annoying

--- startApp.sh
+++ startApp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-env EMAIL=$MYERS_EMAIL hugo server  -w #-t hugo-now-ui #gohugo-theme-ananke
+env EMAIL=$MYERS_EMAIL hugo server -w --disableFastRender #-t hugo-now-ui #gohugo-theme-ananke
